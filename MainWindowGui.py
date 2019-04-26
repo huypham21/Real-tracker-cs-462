72cc2c0c997112ab52ae3573a82da454771b21f7
Jadie <jmar016@tamu.edu>
2019-04-26 13:54:30 -0500

    GUI fully functional - now adding backend

--- MainWindowGui.py
+++ MainWindowGui.py
@@ -9,10 +9,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import datetime
 #Default values for counter system
 roomOcc = 200 
 EvName = "Event Name"
-
+avg = 0
+currOcc = 0
+maxOcc = 0
 
 class Ui_Options(object):
     def setupUi(self, Dialog):
@@ -71,7 +74,7 @@ class Ui_New(object):
         Dialog.setObjectName("Dialog")
         Dialog.resize(500, 500)
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(0, 0, 200, 50))
+        self.label.setGeometry(QtCore.QRect(0, 35, 200, 50))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label.setFont(font)
@@ -79,37 +82,37 @@ class Ui_New(object):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(0, 55, 200, 50))
+        self.label_2.setGeometry(QtCore.QRect(0, 90, 200, 50))
         font = QtGui.QFont()
-        font.setPointSize(12)
+        font.setPointSize(10)
         self.label_2.setFont(font)
         self.label_2.setAutoFillBackground(True)
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(0, 110, 200, 50))
+        self.label_3.setGeometry(QtCore.QRect(0, 145, 200, 50))
         font = QtGui.QFont()
-        font.setPointSize(12)
+        font.setPointSize(8)
         self.label_3.setFont(font)
         self.label_3.setAutoFillBackground(True)
         self.label_3.setAlignment(QtCore.Qt.AlignCenter)
         self.label_3.setObjectName("label_3")
         self.lcdNumber_Count = QtWidgets.QLCDNumber(Dialog)
-        self.lcdNumber_Count.setGeometry(QtCore.QRect(200, 0, 50, 50))
+        self.lcdNumber_Count.setGeometry(QtCore.QRect(200, 35, 50, 50))
         self.lcdNumber_Count.setObjectName("lcdNumber_Count")
         self.lcdNumber_Total = QtWidgets.QLCDNumber(Dialog)
-        self.lcdNumber_Total.setGeometry(QtCore.QRect(200, 55, 50, 50))
+        self.lcdNumber_Total.setGeometry(QtCore.QRect(200, 90, 50, 50))
         self.lcdNumber_Total.setObjectName("lcdNumber_Total")
         self.lcdNumber_Average = QtWidgets.QLCDNumber(Dialog)
-        self.lcdNumber_Average.setGeometry(QtCore.QRect(200, 110, 50, 50))
+        self.lcdNumber_Average.setGeometry(QtCore.QRect(200, 145, 50, 50))
         self.lcdNumber_Average.setObjectName("lcdNumber_Average")
         self.progressBar = QtWidgets.QProgressBar(Dialog)
-        self.progressBar.setGeometry(QtCore.QRect(0, 180, 250, 50))
+        self.progressBar.setGeometry(QtCore.QRect(0, 235, 300, 50))
         self.progressBar.setStatusTip("")
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
         self.label_4 = QtWidgets.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(0, 160, 200, 20))
+        self.label_4.setGeometry(QtCore.QRect(0, 195, 300, 40))
         font = QtGui.QFont()
         font.setPointSize(9)
         self.label_4.setFont(font)
@@ -117,21 +120,29 @@ class Ui_New(object):
         self.label_4.setAlignment(QtCore.Qt.AlignCenter)
         self.label_4.setObjectName("label_4")
         self.optionsButton = QtWidgets.QPushButton(Dialog)
-        self.optionsButton.setGeometry(QtCore.QRect(0, 250, 200, 25))
+        self.optionsButton.setGeometry(QtCore.QRect(0, 285, 300, 50))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.optionsButton.setFont(font)
         self.optionsButton.setObjectName("optionsButton")
         self.quitButton = QtWidgets.QPushButton(Dialog)
-        self.quitButton.setGeometry(QtCore.QRect(0, 275, 200, 35))
+        self.quitButton.setGeometry(QtCore.QRect(0, 335, 300, 50))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.quitButton.setFont(font)
         self.quitButton.setObjectName("quitButton")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(0, 0, 300, 35))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_5.setFont(font)
+        self.label_5.setAutoFillBackground(True)
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_5.setObjectName("label_5")
 
         self.retranslateUi(Dialog)
-        #self.optionsButton.clicked.
-        self.quitButton.clicked.connect(Dialog.close)
+        self.quitButton.clicked.connect(lambda: self.save(Dialog))
+        self.optionsButton.clicked.connect(self.optionMenu)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
@@ -141,9 +152,30 @@ class Ui_New(object):
         self.label_2.setText(_translate("Dialog", "Total Attendance:"))
         self.label_3.setText(_translate("Dialog", "Average Number Present:"))
         self.progressBar.setWhatsThis(_translate("Dialog", "Room Capacity"))
-        self.label_4.setText(_translate("Dialog", "Room Occupancy (Compared to full capacity):"))
+        self.label_4.setText(_translate("Dialog", "Room Occupancy:"))
         self.optionsButton.setText(_translate("Dialog", "Options"))
-        self.quitButton.setText(_translate("Dialog", "Quit"))
+        self.quitButton.setText(_translate("Dialog", "Save and Quit"))
+        self.label_5.setText(_translate("Dialog", "Event Name"))
+    
+    def save(self, Dialog):
+        file = open('data.json', 'a')
+        date = datetime.datetime.now()
+        toWrite = '{"EventName": '+str(EvName) +', "Time": ' +str(date) +', "EndAttend": ' +str(currOcc) +', "AvgAttend": ' +str(avg) +', "MaxAttend": ' +str(maxOcc) +', "RoomOcc": ' +str(roomOcc) +'}\n'
+        file.write(toWrite)
+        print("Wrote" +toWrite)
+        file.close()
+        Dialog.close()
+    
+    def end(self, Dialog):
+        self.save
+        Dialog.close()
+    
+    def optionMenu(self):
+        opt = QtWidgets.QDialog()
+        options = Ui_Options()
+        options.setupUi(opt)
+        opt.exec_()
+    
 ############################################################################
 
 
